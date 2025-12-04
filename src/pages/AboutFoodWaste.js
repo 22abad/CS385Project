12b11db37537a5d0842ec5deab5d134d9c538d1e
CrowCodes <167340832+gitamlilly@users.noreply.github.com>
2025-12-04 18:37:17 +0000

    Added Food Icons for facts

--- src/pages/AboutFoodWaste.js
+++ src/pages/AboutFoodWaste.js
@@ -1,8 +1,29 @@
+import "../styles.css";
+
 export default function AboutFoodWaste() {
   return (
     <>
-      <div className="WastePageTitle container-fluid">
-        <h1>About Food Waste</h1>
+      {/* background image */}
+      <div className="bg-image WasteTitleImage">
+        <div class="mask">
+          <div class="d-flex justify-content-center align-items-center h-100">
+            <h1 class="mb-0 WasteTitle">About Food Waste</h1>
+          </div>
+        </div>
+      </div>
+      <div className="main">
+
+      <div class="container text-center">
+          <div class="row">
+            <div class="col">
+              Column
+            </div>
+            <div class="col">
+              Column
+            </div>
+        </div>
+      </div>
+
       </div>
     </>
   );
