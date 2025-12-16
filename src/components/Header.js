776926921453ca43cc0134a1c23ec91d08ce7d93
nomont49 <42555032+nomont49@users.noreply.github.com>
2025-12-16 09:33:02 -0500

    Change About Us link to lowercase path

--- src/components/Header.js
+++ src/components/Header.js
@@ -46,7 +46,7 @@ function Header() {
                 </li>
               )}
               <li className="nav-item">
-                <Link to="/AboutUs" className="nav-link">
+                <Link to="/about" className="nav-link">
                   About Us
                 </Link>
               </li>
