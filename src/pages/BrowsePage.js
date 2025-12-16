c5ffb6f178064d3f27c3ba7cb07ad93a0afa6d2d
IrishDevNoob <dong@22abad.com>
2025-12-16 17:44:00 +0000

    Update BrowsePage.js

--- src/pages/BrowsePage.js
+++ src/pages/BrowsePage.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Browse from "../Browse";
-import AddDataButton from "../components/AddDataButton";
 
 export default function BrowsePage() {
   return (
     <div className="container mt-5 pt-5">
-      <AddDataButton />
       <Browse />
     </div>
   );
