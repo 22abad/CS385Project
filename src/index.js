ea72977b3fc7aeded7ae09453a2b0abd172358eb
IrishDevNoob <dong@22abad.com>
2025-12-16 17:25:07 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import { CartProvider } from "./contexts/CartContext";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
@@ -15,7 +17,11 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>
 );
